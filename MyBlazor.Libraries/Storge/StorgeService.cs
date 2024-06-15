45bce3551eab671a172e1dc5f4c73f1ccf9cf5de
Mirkhan2 <mirkhan.shams2@gmail.com>
2024-06-15 19:12:44 +0200

    bll

--- MyBlazor.Libraries/Storge/StorgeService.cs
+++ MyBlazor.Libraries/Storge/StorgeService.cs
@@ -7,23 +7,32 @@ using MyBlazor.Libraries.Product.Models;
 
 namespace MyBlazor.Libraries.Storge
 {
-	public class StorgeService : IStorgeService
+	public class StorageService : IStorgeService
 	{
 		public IList<ProductModel> products { get; private set; }
 
-        public StorgeService()
-        {
-            
-        }
-        private void AddProduct(ProductModel product)
-        {
-           //his.products.Add(product);
-            if (!products.Any(p  => p.Sku == product.Sku))
-            {
-                products.Add(product);
-            }
-          
+		public StorageService()
+		{
+			products = new List<ProductModel>();
 
-        }
-    }
+			// Store a list of all the products for the online shop.
+			AddProduct(new ProductModel("BUBBLES-GUMBALL-APRON", "A Gumball for Your Thoughts Apron", 24, "bubbles-gumball-apron-black.jpg"));
+			AddProduct(new ProductModel("REX-MICROCONTROLLERS-APRON", "Great Microcontrollers Think Alike Apron", 24, "rex-microcontrollers-apron-black.jpg"));
+			AddProduct(new ProductModel("DOLORES-COMPUTE-BASEBALLHAT", "I Compute, Therefore I Am Baseball Hat", 29, "dolores-compute-baseballhat-black.jpg"));
+			AddProduct(new ProductModel("BUBBLES-GUMBALL-BASEBALLHAT", "A Gumball for Your Thoughts Baseball Hat", 29, "bubbles-gumball-baseballhat-black.jpg"));
+			AddProduct(new ProductModel("REX-MICROCONTROLLERS-BASEBALLHAT", "Great Microcontrollers Think Alike Baseball Hat", 29, "rex-microcontrollers-baseballhat-black.jpg"));
+			AddProduct(new ProductModel("DOLORES-COMPUTE-MUG", "I Compute, Therefore I Am Mug", 16, "dolores-compute-mug-black.jpg"));
+			AddProduct(new ProductModel("DOLORES-COMPUTE-TSHIRT", "I Compute, Therefore I Am T-shirt", 26, "dolores-compute-tshirt-black.jpg"));
+			AddProduct(new ProductModel("REX-MICROCONTROLLERS-TSHIRT", "Great Microcontrollers Think Alike T-shirt", 26, "rex-microcontrollers-tshirt-black.jpg"));
+		}
+
+		private void AddProduct(ProductModel product)
+		{
+			if (!products.Any(p => p.Sku == product.Sku))
+			{
+
+				products.Add(product);
+			}
+		}
+	}
 }
--- MyBlazor/Program.cs
+++ MyBlazor/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazor;
+using MyBlazor.Libraries.Product;
+using MyBlazor.Libraries.Storge;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Services.AddTransient<IStorgeService, StorageService>();
 
 await builder.Build().RunAsync();
