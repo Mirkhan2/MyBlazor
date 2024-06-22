4a1f17353383cf8d374c6a3c10687518bbc39641
Mirkhan2 <mirkhan.shams2@gmail.com>
2024-06-22 13:40:52 +0200

    update

---
+++ MyBlazor/Pages/ShoppingCart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBlazor.Pages
+{
+    public class ShoppingCartModelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
